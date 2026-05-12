d2dec3d1451166a2e54123d7063939aeea2ee4ac
Ronjableh <jannik.frider@gmail.com>
2026-05-12 20:43:49 +0200

    Add files via upload

---
+++ colors.lua
@@ -0,0 +1,11 @@
+hl.config({
+    general = {
+        col = {
+            active_border   = "rgba(167, 192, 222, 1)",
+            inactive_border = "rgba(16, 10, 80, 1)",
+        },
+    },
+    misc = {
+        background_color = "rgba(0, 0, 0, 0)",
+    },
+})
---
+++ hyprland.lua
@@ -0,0 +1,18 @@
+-- This file sources other files in `hyprland` and `custom` folders
+-- You wanna add your stuff in files in `custom`
+
+-- Environment variables --
+-- require("hyprland/env")
+--require("custom/env")
+
+-- Defaults --
+require("hyprland/execs")
+require("hyprland/general")
+require("hyprland/rules")
+require("hyprland/colors")
+require("hyprland/keybinds")
+
+
+local terminal    = "kitty"
+local fileManager = "dolphin"
+local menu        = "hyprlauncher"
