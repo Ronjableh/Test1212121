7903605316f8e7556ff96791c87f77c8992c0d6e
Ronjableh <jannik.frider@gmail.com>
2026-05-12 21:15:50 +0200

    Create globals.lua

---
+++ .config/hypr/custom/globals.lua
@@ -0,0 +1,5 @@
+terminal    = "kitty"
+fileManager = "dolphin"
+menu        = "hyprlauncher"
+mainMod = "SUPER" -- Sets "Windows" key as main modifier
+closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
